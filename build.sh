9eec80a0153f35d0cd3b6528a22fd0204031963c
L5ks8 <L5ks8@users.noreply.github.com>
2026-08-16 21:23:55 +0200

    Add frontend build step to build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ apt-get update && apt-get install -y --no-install-recommends ffmpeg
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+cd Website/frontend
+npm install
+npm run build
+cd ../..
