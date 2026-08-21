935c649976fe6425339aafb92fff79c3db892a3e
L5ks8 <L5ks8@users.noreply.github.com>
2026-08-21 13:32:30 +0200

    Install tailwindcss v4 to fix Moderation.jsx styling

--- Website/frontend/src/main.jsx
+++ Website/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './styles/tailwind.css'
 import './styles/variables.css'
 import './styles/global.css'
 import './styles/animations.css'
